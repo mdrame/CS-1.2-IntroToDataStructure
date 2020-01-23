05254c34da39335504add14c9fce2d161162da6b
mohammed <mdrame113@gmail.com>
2020-01-23 15:45:21 -0800

    Added python_quote module

--- app.py
+++ app.py
@@ -15,12 +15,21 @@ quotes = ("It's just a flesh wound.",
           "He's not the Messiah. He's a very naughty boy!",
           "THIS IS AN EX-PARROT!!")
 
+def random_Number_Processing () :
 # picked random index from the quotes array/list
-randomPick = randint(0, len(quotes) - 1)
+    randomPick = randint(0, len(quotes) - 1)
 # test
-print(f"Random Number: {randomPick}")
+    # print(f"Random Number: {randomPick}")
 
 # assing selected quote to a new variable
-selectedTweet = quotes[randomPick]
+    selectedTweet = quotes[randomPick]
 # test 
-print(f"Selected Quote: {selectedTweet}")
+    # print(f"Selected Quote: {selectedTweet}")
+    return selectedTweet
+
+
+
+
+if __name__ == '__main__':
+    quote = random_Number_Processing()
+    print(f"Quote Selected: {quote}")
