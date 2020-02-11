43f44bbcd2a6f3d912bd3e7d679721b89696a2fa
mohammed <mdrame113@gmail.com>
2020-02-10 21:48:20 -0800

    git pull origin master

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-# cs 1.2 introduction to data structure
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
+
+app = Flask(__name__)
 from random import randint, choice
 
 
@@ -11,25 +14,31 @@ from random import randint, choice
 
 
 # List of worlds 
-quotes = ("It's just a flesh wound.",
-          "He's not the Messiah. He's a very naughty boy!",
-          "THIS IS AN EX-PARROT!!")
+# quotes = ("It's just a flesh wound.",
+#           "He's not the Messiah. He's a very naughty boy!",
+#           "THIS IS AN EX-PARROT!!")
 
-def random_Number_Processing () :
-# picked random index from the quotes array/list
-    randomPick = randint(0, len(quotes) - 1)
-# test
-    # print(f"Random Number: {randomPick}")
+# def random_Number_Processing () :
+# # picked random index from the quotes array/list
+#     randomPick = randint(0, len(quotes) - 1)
+# # test
+#     # print(f"Random Number: {randomPick}")
 
-# assing selected quote to a new variable
-    selectedTweet = quotes[randomPick]
-# test 
-    # print(f"Selected Quote: {selectedTweet}")
-    return selectedTweet
+# # assing selected quote to a new variable
+#     selectedTweet = quotes[randomPick]
+# # test 
+#     # print(f"Selected Quote: {selectedTweet}")
+#     return selectedTweet
+
+@app.route('/') #index
+def dashBoard():
+
+
+    return render_template("index.html")
+
 
 
 
 
 if __name__ == '__main__':
-    quote = random_Number_Processing()
-    print(f"Quote Selected: {quote}")
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
