76925ace7a5361b36439713130dc5097780515f1
mohammed <mdrame113@gmail.com>
2020-03-03 21:46:50 -0800

    video change

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
-from histogram import histogram_dict, read_file
-from markov_chain import higher_order, higher_order_walk, new_chain, create_sentence, order_sample, cleanup_text_file
+from flask import Flask, render_template
 import random
+from markov_chain import higher_order, higher_order_walk, new_chain, create_sentence, order_sample, cleanup_text_file
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def show_phrase():
+def dashBoard():
     words = cleanup_text_file('txt_files/houseofquiet.txt')
     word_list = words.split()
     sentence = higher_order_walk(word_list, 40)
